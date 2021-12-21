2cff172005ee132970c3db8641687b4a9c73953f
Vladimir Zelenko <vzelenko@gmail.com>
2021-12-20 22:52:44 -0500

    Add passport and session

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
