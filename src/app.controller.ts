c16d36789e1a36968813716f0ebc893c6da9d5bd
Vladimir Zelenko <vzelenko@gmail.com>
2021-12-20 23:01:35 -0500

    Add guards to login routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,16 @@
-// src/app.controller.ts
-import { Controller, Get, Post, Res, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Res,
+  Render,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
+
+import { LoginGuard } from './common/guards/login.guard';
+import { AuthenticatedGuard } from './common/guards/authenticated.guard';
 
 @Controller()
 export class AppController {
@@ -10,25 +20,29 @@ export class AppController {
     return;
   }
 
+  @UseGuards(LoginGuard)
   @Post('/login')
-  login(@Res() res: Response): void {
+  login(@Res() res: Response) {
     res.redirect('/home');
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Get('/home')
   @Render('home')
-  getHome() {
-    return;
+  getHome(@Request() req) {
+    return { user: req.user };
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Get('/profile')
   @Render('profile')
-  getProfile() {
-    return;
+  getProfile(@Request() req) {
+    return { user: req.user };
   }
 
   @Get('/logout')
-  logout(@Res() res: Response): void {
+  logout(@Request() req, @Res() res: Response) {
+    req.logout();
     res.redirect('/');
   }
 }
