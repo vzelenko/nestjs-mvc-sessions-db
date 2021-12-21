9483d78ed328a9a02f0bd917a51e118566a9d2ca
Vladimir Zelenko <vzelenko@gmail.com>
2021-12-20 22:58:40 -0500

    Added passport session support

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+
+import * as session from 'express-session';
+import flash = require('connect-flash');
 import { engine } from 'express-handlebars';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,6 +18,18 @@ async function bootstrap() {
   app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
   app.set('view engine', 'hbs');
   // app.enable('view cache');
+
+  app.use(
+    session({
+      secret: 'nest cats',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
 
   await app.listen(3000);
 }
